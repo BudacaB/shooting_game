ab77056dcc41388fae50e9affae31146adf06f17
andrei <andreimcristof@gmail.com>
2019-01-26 18:36:07 +0100

    t

---
+++ Assets/FadeOutScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class FadeOutScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //var t = this.GetComponent<TextMeshProUGUI>();
+        //Debug.Log(t);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/TargetPractice.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TargetPractice : MonoBehaviour
+{
+    public int Health { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.Health = 99999999;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override string ToString()
+    {
+        return $"Target has {this.Health} left";
+    }
+
+    internal void TakeDamage(int incomingDamage)
+    {
+        this.Health = this.Health - incomingDamage;
+        
+
+        GameObject canvasObject = GameObject.FindGameObjectWithTag("Canvas");
+        var txtDmgTaken = canvasObject.transform.Find("TextDamageTaken");
+        var componentTxtDaamgetaken = txtDmgTaken.GetComponent<TextMeshProUGUI>();
+        componentTxtDaamgetaken.SetText($"{incomingDamage}");
+
+        StartCoroutine(FadeTextToZeroAlpha(.2f, componentTxtDaamgetaken));
+
+        var txtRemainingHealth = canvasObject.transform.Find("TextRemainingHealth");
+        var componentTxtRemainingHealth = txtRemainingHealth.GetComponent<TextMeshProUGUI>();
+        componentTxtRemainingHealth.SetText(this.ToString());
+    }
+
+    public IEnumerator FadeTextToZeroAlpha(float t, TextMeshProUGUI i)
+    {
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 1);
+        while (i.color.a > 0.0f)
+        {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a - (Time.deltaTime / t));
+            yield return null;
+        }
+    }
+}
